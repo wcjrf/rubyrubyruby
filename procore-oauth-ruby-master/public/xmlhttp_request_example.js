e7531c1bc8560f2b55b87dbd07eeb45c4988ec0a
wcjrf <37749046+wcjrf@users.noreply.github.com>
2018-03-30 09:47:37 -0700

    ds

--- procore-oauth-ruby-master/public/xmlhttp_request_example.js
+++ procore-oauth-ruby-master/public/xmlhttp_request_example.js
@@ -1,4 +1,5 @@
-var logResponse = function (http) {
+var logResponse;
+logResponse = function (http) {
   return function() {
     console.info('per-page ->', parseInt(http.getResponseHeader('per-page')))
     element = document.getElementById('api-log')
@@ -14,46 +15,10 @@ var apiRequest = function(path, method) {
   path = path || '/vapid/companies'
   console.info(method, apiUrl + path)
   var httpRequest = new XMLHttpRequest()
-    httpRequest.withCredentials = true;
   httpRequest.open(method, apiUrl + path, true)
   httpRequest.setRequestHeader('Authorization', 'Bearer '+ localStorage.getItem('accessToken'))
   httpRequest.onreadystatechange = logResponse(httpRequest)
   httpRequest.send()
-  httpRequest.onload = function() {
-    var companyidText = httpRequest.response; // get the string from the response
-    var companyid = JSON.parse(companyidText); // convert it to an object
-    populateHeader(companyid);
-    showid(companyid);
 }
-
-logResponse.open('GET', apiUrl + 'vapid/companies');
-logResponse.responseType = 'text'; // now we're getting a string!
-logResponse.send();
-
-logResponse.onload = function() {
-  var companyidText = logResponse.response; // get the string from the response
-  var companyid = JSON.parse(companyidText); // convert it to an object
-  populateHeader(companyid);
-  showid(companyid);
-}
-
-httpRequest.onload = function() {
-  var id = httpRequest.response;
-  populateHeader(id);
-  showid(id);
-}
-
-
-
-function populateHeader(jsonObj) {
-  var seattleid = document.createElement('seattleid');
-  myseattleid.textContent = jsonObj['id'];
-  header.appendChild(myH1);
-
-  var myPara = document.createElement('p');
-  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-  header.appendChild(myPara);
-}
-
 
 apiRequest('/vapid/companies')
