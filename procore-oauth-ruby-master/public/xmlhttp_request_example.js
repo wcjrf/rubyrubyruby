826e5b6fd1573bcf6ed99a78091e8fad5491e42a
wcjrf <37749046+wcjrf@users.noreply.github.com>
2018-03-29 21:20:20 -0700

    6

--- procore-oauth-ruby-master/public/xmlhttp_request_example.js
+++ procore-oauth-ruby-master/public/xmlhttp_request_example.js
@@ -15,7 +15,7 @@ var apiRequest = function(path, method) {
   console.info(method, apiUrl + path)
   var httpRequest = new XMLHttpRequest()
   httpRequest.open(method, apiUrl + path, true)
-    httpRequest.withCredentials = true;
+  httpRequest.withCredentials = 'true';
   httpRequest.setRequestHeader('Authorization', 'Bearer '+ localStorage.getItem('accessToken'))
   httpRequest.onreadystatechange = logResponse(httpRequest)
   httpRequest.send(data)
